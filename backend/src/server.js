07b68aba41556739c4c8407feddcf0c3bd8a41b9
Hemant Singh <hemant.kr.sin9h@gmail.com>
2026-01-15 15:48:45 +0530

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+//const express = require('express');
+
+import express from 'express';
+import dotenv from 'dotenv';
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+dotenv.config()
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/api/auth', authRoutes); /* 
+/api/auth send it to authRoutes */
+
+app.use('/api/messages', messageRoutes);
+
+app.listen(PORT, () => {
+  console.log('server running on port: ' + PORT)
+});
