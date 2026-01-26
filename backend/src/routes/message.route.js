629e75da477cd6b8891d669e66add64cc778547d
Hemant Singh <hemant.kr.sin9h@gmail.com>
2026-01-26 18:55:34 +0530

    Add message routes

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,18 @@
 import express from 'express';
+import { getAllcontacts, getChatPartners, getMessagesByUserId, sendMessage } from '../controllers/message.controller.js';
+import { protectedRoute } from '../middleware/auth.middleware.js';
+import { arcjetProtection } from '../middleware/arcjet.middleware.js';
 
 const router = express.Router();
 
-router.get('/send',(req,res)=>{
-  res.send('send Message endpoint')
-})
+
+router.use(arcjetProtection, protectedRoute); // req.user will be supplied protectedRoute to the controllers
+
+
+// order of the routes are important.
+router.get('/contacts', getAllcontacts);
+router.get('/chats', getChatPartners);
+router.get('/:id', getMessagesByUserId); 
+router.post('/send/:id',sendMessage)
 
 export default router;
